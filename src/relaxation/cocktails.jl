c77863ef3637c40654b53b3974d53936e6a39f47
Thomas2515 <lok.ng.24@ucl.ac.uk>
2025-08-29 13:40:20 +0100

    alter embeddings calculation and save results

--- src/relaxation/cocktails.jl
+++ src/relaxation/cocktails.jl
@@ -1,7 +1,15 @@
 function prepcocktails(cocktails)
-    embeddings = get_embeddings(cocktails)
     # input: cocktails where peaks are BasicPeaks
-    # output: cocktails where peaks are ScreeningPeaks, also contains UMAP coordinates
+    # output: cocktails where peaks are ScreeningPeaks, also initialize the UMAP coordinates fields
+    for cocktail in cocktails
+        for (i, peak) in enumerate(cocktail.peaks)
+            newpeak = ScreeningPeak(peak)
+            cocktail.peaks[i] = newpeak
+        end
+        sort!(cocktail.peaks, by=sp -> sp.reference_shift, rev=true)
+    end
+    
+    embeddings = get_embeddings(cocktails)
     peak_count = 0
     for cocktail in cocktails
         for (i, peak) in enumerate(cocktail.peaks)
@@ -9,28 +17,35 @@ function prepcocktails(cocktails)
             newpeak = ScreeningPeak(peak, embeddings[1, peak_count], embeddings[2, peak_count])
             cocktail.peaks[i] = newpeak
         end
-        sort!(cocktail.peaks, by=sp -> sp.reference_shift, rev=true)
     end
-
     return cocktails
 end
 
 function get_embeddings(cocktails)
     smiles = []
+    DeltaR2s = []
     for cocktail in cocktails
         for peak in cocktail.peaks
             push!(smiles, peak.smiles)
+            push!(DeltaR2s, Measurements.value(DeltaR2(peak)))
         end
     end
     fingerprint_matrix = []
-    for i in smiles
-        mol = get_mol(i)
+    for (i, entry) in enumerate(smiles)
+        mol = get_mol(entry)
         fingerprint = get_morgan_fp(mol)
         fingerprint_split = split(fingerprint, "")
         fingerprint_parsed = []
         for bits in fingerprint_split
-            push!(fingerprint_parsed, parse.(Float64, bits))
+            push!(fingerprint_parsed, parse(Float64, bits))
         end
+        max_DeltaR2 = maximum(DeltaR2s)
+        if DeltaR2s[i] < 0.0
+            DeltaR2_factor = 0.0
+        else
+            DeltaR2_factor = (DeltaR2s[i] / max_DeltaR2) * 20.0
+        end
+        push!(fingerprint_parsed, DeltaR2_factor)
         push!(fingerprint_matrix, fingerprint_parsed)
     end
     fingerprint_matrix = hcat(fingerprint_matrix...)
--- src/relaxation/screeningpeaks.jl
+++ src/relaxation/screeningpeaks.jl
@@ -39,15 +39,15 @@
 # end
 
 
-function ScreeningPeak(peak::BasicPeak, x, y)
+function ScreeningPeak(peak::BasicPeak)
     refspec = peak.refspec
     boundspec = peak.boundspec
     peak_id = peak.id
     fragment_id = peak.fragment_id #peak_id[1:end-1]
     smiles = peak.smiles
     cocktail_id = peak.cocktail_id
-    umap_x = x
-    umap_y = y
+    umap_x = 0.0
+    umap_y = 0.0
     library_shift = peak.library_shift
     reference_shift = peak.ref_shift
     bound_shift = peak.bound_shift
@@ -132,4 +132,42 @@ end
 
 function reducedchi2(sp::ScreeningPeak)
     (sp.reference_relaxation.reducedchi2 + sp.bound_relaxation.reducedchi2) / 2
+end
+
+function ScreeningPeak(sp::ScreeningPeak, x, y)
+    refspec = sp.refspec
+    boundspec = sp.boundspec
+    peak_id = sp.peak_id
+    fragment_id = sp.fragment_id
+    smiles = sp.smiles
+    cocktail_id = sp.cocktail_id
+    umap_x = x
+    umap_y = y
+    library_shift = sp.library_shift
+    reference_shift = sp.reference_shift
+    bound_shift = sp.bound_shift
+    reference_intensity = sp.reference_intensity
+    reference_intensity_error = sp.reference_intensity_error
+    bound_intensity = sp.bound_intensity
+    bound_intensity_error = sp.bound_intensity_error
+    reference_relaxation = sp.reference_relaxation
+    bound_relaxation = sp.bound_relaxation
+
+    ScreeningPeak(refspec,
+        boundspec,
+        peak_id,
+        fragment_id,
+        smiles,
+        cocktail_id,
+        umap_x,
+        umap_y,
+        library_shift,
+        reference_shift,
+        bound_shift,
+        reference_intensity,
+        reference_intensity_error,
+        bound_intensity,
+        bound_intensity_error,
+        reference_relaxation,
+        bound_relaxation)
 end
